fe7c9f537a44181b7a419cf1c10eecfd5ff8314d
Lu Dang <eclipselu@gmail.com>
2018-01-14 13:53:02 -0500

    initial commit

---
+++ init.el
@@ -0,0 +1,20 @@
+(require 'package)
+
+;;; Install into separate package dirs for each Emacs version, to prevent bytecode incompatibility
+(let ((versioned-package-dir
+       (expand-file-name (format "elpa-%s.%s" emacs-major-version emacs-minor-version)
+                         user-emacs-directory)))
+  (setq package-user-dir versioned-package-dir))
+
+;;; Standard package repositories
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t))
+
+;; We include the org repository for completeness, but don't normally
+;; use it.
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+
+(org-babel-load-file "~/.emacs.d/configuration.org")
+(put 'narrow-to-region 'disabled nil)
